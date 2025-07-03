3cde39fe21f11168d2ec58eabd34e2855e2acf9c
Denis Ryzhkov <denisr@denisr.com>
2025-07-03 14:21:39 +0200

    fix: Do not replace `expr` with int zero `0` if `expr` is not overridden (#380)

--- kof-operator/internal/controller/configmap_controller.go
+++ kof-operator/internal/controller/configmap_controller.go
@@ -383,7 +383,7 @@ func unmarshalRules[T AlertRules | RecordRules](
 
 // Patch `oldRule` with `newRule`.
 func patchRule(oldRule *promv1.Rule, newRule *promv1.Rule) {
-	if newRule.Expr.String() != "" {
+	if !(newRule.Expr.StrVal == "" && newRule.Expr.IntVal == 0) {
 		oldRule.Expr = newRule.Expr
 	}
 	if newRule.For != nil {

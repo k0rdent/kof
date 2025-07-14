f81bea6faa6db20b04b3b5529f183835db2dca43
Andrejs <andrejs.pon00@gmail.com>
2025-07-14 18:18:19 +0300

    Add stat row with trends component

---
+++ kof-operator/webapp/collector/src/components/shared/StatRowWithTrend.tsx
@@ -0,0 +1,53 @@
+import { Trend } from "@/providers/collectors_metrics/CollectorsMetricsRecordManager";
+import { TrendingUp } from "lucide-react";
+import { JSX } from "react";
+
+interface StatRowProps {
+  text: string;
+  textStyles?: string;
+  value: string | number;
+  valueStyles?: string;
+  containerStyle?: string;
+  trend: Trend | null;
+  isPositiveTrend?: boolean;
+}
+
+const StatRowWithTrend = ({
+  text,
+  value,
+  textStyles,
+  valueStyles,
+  containerStyle,
+  trend,
+  isPositiveTrend = true,
+}: StatRowProps): JSX.Element => {
+  let trendMessageColor: string | undefined;
+
+  if (isPositiveTrend && trend?.isTrending) {
+    trendMessageColor = "text-green-600";
+  } else if (!isPositiveTrend && !trend?.isTrending) {
+    trendMessageColor = "text-green-600";
+  } else {
+    trendMessageColor = "text-red-600";
+  }
+
+  return (
+    <div>
+      <div className={`flex justify-between mb-0 ${containerStyle}`}>
+        <span className={`text-sm ${textStyles}`}>{text}</span>
+        {trend && (
+          <div
+            className={`flex gap-2 items-center font-sm font-medium ${trendMessageColor}`}
+          >
+            {trend.isTrending && <TrendingUp className="w-5 h-5" />}
+            {trend.message}
+          </div>
+        )}
+      </div>
+      <div className="flex justify-end">
+        <span className={`text-sm ${valueStyles}`}>{value}</span>
+      </div>
+    </div>
+  );
+};
+export default StatRowWithTrend;

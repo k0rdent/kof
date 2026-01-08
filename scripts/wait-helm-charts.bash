a5456bf5ea3faada3fbb328784d3b70883bd77a0
Aleksei Larkov <alarkov@mirantis.com>
2026-01-08 15:17:04 +0200

    ci: test Istio upgrade (#680)
    * ci: test istio upgrade
    
    * Increase wait script timeout from 20 to 30 seconds
    
    ---------
    
    Co-authored-by: Andrejs <andrejs.pon00@gmail.com>

--- scripts/wait-helm-charts.bash
+++ scripts/wait-helm-charts.bash
@@ -14,7 +14,7 @@ for attempt in $(seq 1 20); do
     helm_status=$($HELM list --kube-context "$CONTEXT" -A --deployed --pending -o yaml | $YQ ".[] | select(.name == \"$name\") | .status")
     if [ ! "$helm_status" = "deployed" ]; then
       echo "$attempt: Waiting for the $name helm chart status to be deployed. Current: [$helm_status]"
-      sleep 20
+      sleep 30
       deployed="false"
       break
     fi
@@ -27,7 +27,7 @@ for attempt in $(seq 1 20); do
     )
     if [[ "$expected_version" != "$actual_version" ]]; then
       echo "$attempt: Waiting for the $name helm chart to be upgraded to $expected_version. Current: $actual_version"
-      sleep 20
+      sleep 30
       deployed="false"
       break
     fi

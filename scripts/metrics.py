8e2cb799be1bca51c90b0b10b814d602c04501e7
Andrejs <andrejs.pon00@gmail.com>
2026-01-23 15:02:52 +0200

    ci: add tenant isolation test

--- scripts/metrics.py
+++ scripts/metrics.py
@@ -6,20 +6,40 @@ child and regional adopted clusters
 
 import sys
 import time
+import argparse
 
 import requests
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument(
+    "--child-labels",
+    type=str,
+    help="Additional label selectors to filter metrics in child cluster queries",
+)
+
+parser.add_argument(
+    "--regional-labels",
+    type=str,
+    help="Additional label selectors to filter metrics in regional cluster queries",
+)
+
+args = parser.parse_args()
 
 REGIONAL_CLUSTER = "regional-adopted"
 CHILD_CLUSTER = "child-adopted"
 
+CHILD_LABELS = f", {args.child_labels}" if args.child_labels else ""
+REGIONAL_LABELS = f", {args.regional_labels}" if args.regional_labels else ""
+
 metrics = [
-    f'up{{job=~"kubernetes-apiservers|apiserver", cluster="{CHILD_CLUSTER}"}}',
-    f'up{{job=~"kubernetes-apiservers|apiserver", cluster="{REGIONAL_CLUSTER}"}}',
-    f'up{{app_kubernetes_io_name="kof-collectors-daemon-collector", cluster="{CHILD_CLUSTER}"}}',
-    f'up{{app_kubernetes_io_name="kof-collectors-daemon-collector", cluster="{REGIONAL_CLUSTER}"}}',
-    f'vm_app_uptime_seconds{{cluster="{REGIONAL_CLUSTER}"}}',
-    f'sum(node_total_hourly_cost{{cluster="{CHILD_CLUSTER}"}})',
-    f'sum(node_total_hourly_cost{{cluster="{REGIONAL_CLUSTER}"}})',
+    f'up{{job=~"kubernetes-apiservers|apiserver", cluster="{CHILD_CLUSTER}"{CHILD_LABELS}}}',
+    f'up{{job=~"kubernetes-apiservers|apiserver", cluster="{REGIONAL_CLUSTER}"{REGIONAL_LABELS}}}',
+    f'up{{app_kubernetes_io_name="kof-collectors-daemon-collector", cluster="{CHILD_CLUSTER}"{CHILD_LABELS}}}',
+    f'up{{app_kubernetes_io_name="kof-collectors-daemon-collector", cluster="{REGIONAL_CLUSTER}"{REGIONAL_LABELS}}}',
+    f'vm_app_uptime_seconds{{cluster="{REGIONAL_CLUSTER}"{REGIONAL_LABELS}}}',
+    f'sum(node_total_hourly_cost{{cluster="{CHILD_CLUSTER}"{CHILD_LABELS}}})',
+    f'sum(node_total_hourly_cost{{cluster="{REGIONAL_CLUSTER}"{REGIONAL_LABELS}}})',
 ]
 
 from_timestamp = time.time() - 60

ff7a8b3c7ed74a69a17d3bf32c2b8c3c3532b1f2
Andrejs Ponatovskis <93515289+AndrejsPon00@users.noreply.github.com>
2026-01-30 17:51:26 +0200

    fix: determine adopted cluster secret name without suffix parsing (#714)
    * fix: remove hardcoded `-kubeconf` suffix for adopted cluster secrets
    
    * rework region handling logic after removing hardcoded suffix
    
    * avoid vm rules propagation on non kcm region cluster

--- kof-operator/internal/k8s/region.go
+++ kof-operator/internal/k8s/region.go
@@ -3,15 +3,21 @@ package k8s
 import (
 	"context"
 	"fmt"
-	"strings"
 
 	kcmv1beta1 "github.com/K0rdent/kcm/api/v1beta1"
 	"github.com/k0rdent/kof/kof-operator/internal/controller/utils"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
+
+type CachedClusterData struct {
+	Regions     *kcmv1beta1.RegionList
+	Credentials *kcmv1beta1.CredentialList
+	Clusters    *kcmv1beta1.ClusterDeploymentList
+}
 
 // If a Kubeconfig secret exists in the management cluster, we assume the cluster is not in the region
 func CreatedInKCMRegion(ctx context.Context, client client.Client, cd *kcmv1beta1.ClusterDeployment) (bool, error) {
@@ -35,7 +41,7 @@ func CreatedInKCMRegion(ctx context.Context, client client.Client, cd *kcmv1beta
 
 	secret := new(corev1.Secret)
 	namespacedName := types.NamespacedName{
-		Name:      GetSecretName(cd),
+		Name:      GetCloudClusterSecretName(cd),
 		Namespace: DefaultSystemNamespace,
 	}
 	err := client.Get(ctx, namespacedName, secret)
@@ -48,25 +54,18 @@ func CreatedInKCMRegion(ctx context.Context, client client.Client, cd *kcmv1beta
 	return false, nil
 }
 
-func CreatedInKCMRegionByName(ctx context.Context, k8sClient client.Client, clusterName string) (bool, error) {
-	clusterSecretName := fmt.Sprintf("%s-%s", clusterName, ClusterSecretSuffix)
-	adoptedClusterSecretName := fmt.Sprintf("%s-%s", clusterName, AdoptedClusterSecretSuffix)
-
-	secretNames := []string{clusterSecretName, adoptedClusterSecretName}
-	kubeconfigSecret := new(corev1.Secret)
-
-	for _, secretName := range secretNames {
-		if err := k8sClient.Get(ctx, types.NamespacedName{
-			Name:      secretName,
-			Namespace: DefaultSystemNamespace,
-		}, kubeconfigSecret); err == nil {
-			return false, nil
-		} else if !errors.IsNotFound(err) {
-			return false, fmt.Errorf("failed to get kubeconfig secret '%s': %v", secretName, err)
-		}
+func IsClusterKcmRegion(ctx context.Context, client client.Client, clusterName, namespace string) (bool, error) {
+	clusters, err := GetKcmRegionClusters(ctx, client)
+	if err != nil {
+		return false, fmt.Errorf("failed to get KCM region clusters: %v", err)
 	}
 
-	return true, nil
+	for _, cluster := range clusters {
+		if cluster.Name == clusterName && cluster.Namespace == namespace {
+			return true, nil
+		}
+	}
+	return false, nil
 }
 
 func IsClusterInSameKcmRegion(ctx context.Context, client client.Client, childName, childNamespace, regionalName, regionalNamespace string) (bool, error) {
@@ -99,7 +98,11 @@ func IsClusterInSameKcmRegion(ctx context.Context, client client.Client, childNa
 		return false, fmt.Errorf("failed to list regions: %v", err)
 	}
 
-	kubeconfigSecretName := GetSecretName(regional)
+	kubeconfigSecretName, err := GetKubeconfigSecretName(ctx, client, regional)
+	if err != nil {
+		return false, fmt.Errorf("failed to get secret name: %v", err)
+	}
+
 	regionName := ""
 	for _, region := range regionList.Items {
 		if region.Spec.KubeConfig != nil && region.Spec.KubeConfig.Name == kubeconfigSecretName {
@@ -132,55 +135,45 @@ func IsClusterInSameKcmRegion(ctx context.Context, client client.Client, childNa
 	return child.Spec.Credential == regionCredsName, nil
 }
 
+// GetClusterDeploymentsInSameKcmRegion returns the list of ClusterDeployments that are in the same KCM region
+// as the specified ClusterDeployment. The specified ClusterDeployment must be a child cluster.
 func GetClusterDeploymentsInSameKcmRegion(ctx context.Context, client client.Client, clusterDeployment *kcmv1beta1.ClusterDeployment) ([]*kcmv1beta1.ClusterDeployment, error) {
-	clusterList := new(kcmv1beta1.ClusterDeploymentList)
-	regionList := new(kcmv1beta1.RegionList)
-	credList := new(kcmv1beta1.CredentialList)
+	cdCred := new(kcmv1beta1.Credential)
+	if err := client.Get(ctx, types.NamespacedName{
+		Name:      clusterDeployment.Spec.Credential,
+		Namespace: clusterDeployment.Namespace,
+	}, cdCred); err != nil {
+		return nil, fmt.Errorf("failed to get cluster deployment credential: %v", err)
+	}
 
+	var result []*kcmv1beta1.ClusterDeployment
+	if cdCred.Spec.Region == "" {
+		return result, nil
+	}
+
+	regionName := cdCred.Spec.Region
+	region := new(kcmv1beta1.Region)
+	if err := client.Get(ctx, types.NamespacedName{Name: regionName}, region); err != nil {
+		return nil, fmt.Errorf("failed to get region %s: %v", regionName, err)
+	}
+
+	clusterList := new(kcmv1beta1.ClusterDeploymentList)
 	if err := client.List(ctx, clusterList); err != nil {
 		return nil, fmt.Errorf("failed to list cluster deployments: %v", err)
 	}
 
-	if err := client.List(ctx, regionList); err != nil {
-		return nil, fmt.Errorf("failed to list regions: %v", err)
-	}
-
-	if err := client.List(ctx, credList); err != nil {
-		return nil, fmt.Errorf("failed to list credentials: %v", err)
-	}
-
-	var result []*kcmv1beta1.ClusterDeployment
-	var regionName string
 	var regionClusterName string
 
-	for _, creds := range credList.Items {
-		if creds.Name == clusterDeployment.Spec.Credential {
-			regionName = creds.Spec.Region
-			break
-		}
+	if region.Spec.ClusterDeployment != nil {
+		regionClusterName = region.Spec.ClusterDeployment.Name
 	}
 
-	for _, region := range regionList.Items {
-		if region.Name != regionName {
-			continue
+	if region.Spec.KubeConfig != nil && region.Spec.KubeConfig.Name != "" {
+		config, err := GetKubeconfigFromSecret(ctx, client, region.Spec.KubeConfig.Name, region.Namespace)
+		if err != nil {
+			return nil, fmt.Errorf("failed to get kubeconfig for region %s: %v", region.Name, err)
 		}
-
-		if region.Spec.ClusterDeployment != nil {
-			regionClusterName = region.Spec.ClusterDeployment.Name
-			break
-		}
-
-		if region.Spec.KubeConfig != nil && region.Spec.KubeConfig.Name != "" {
-			if clusterName, found := strings.CutSuffix(region.Spec.KubeConfig.Name, ClusterSecretSuffix); found {
-				regionClusterName = clusterName
-				break
-			}
-
-			if clusterName, found := strings.CutSuffix(region.Spec.KubeConfig.Name, AdoptedClusterSecretSuffix); found {
-				regionClusterName = clusterName
-				break
-			}
-		}
+		regionClusterName = config.CurrentContext
 	}
 
 	for _, cd := range clusterList.Items {
@@ -195,4 +188,115 @@ func GetClusterDeploymentsInSameKcmRegion(ctx context.Context, client client.Cli
 	}
 
 	return result, nil
+}
+
+// GetKcmRegionChildClusters returns the list of ClusterDeployments that are inside the specified KCM region.
+func GetKcmRegionChildClusters(ctx context.Context, kubeClient client.Client, regionCluster *kcmv1beta1.ClusterDeployment, cache CachedClusterData) ([]*kcmv1beta1.ClusterDeployment, error) {
+	creds := cache.Credentials
+	clusters := cache.Clusters
+
+	region, err := GetRegionByClusterDeployment(ctx, kubeClient, regionCluster, cache)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get region by cluster deployment %s/%s: %w", regionCluster.Namespace, regionCluster.Name, err)
+	}
+
+	if region == nil {
+		return nil, fmt.Errorf("region for cluster %s not found", regionCluster.Name)
+	}
+
+	if creds == nil {
+		creds, err = GetCredentials(ctx, kubeClient)
+		if err != nil {
+			return nil, fmt.Errorf("failed to get credentials: %w", err)
+		}
+	}
+
+	if clusters == nil {
+		clusters, err = GetKofChildClusterDeployments(ctx, kubeClient)
+		if err != nil {
+			return nil, fmt.Errorf("failed to get kof child cluster deployments: %w", err)
+		}
+	}
+
+	regionCredNames := make(map[string]struct{}, len(creds.Items))
+	for _, cred := range creds.Items {
+		if cred.Spec.Region == region.Name {
+			regionCredNames[cred.Name] = struct{}{}
+		}
+	}
+
+	childCds := make([]*kcmv1beta1.ClusterDeployment, 0, len(clusters.Items))
+	for _, childCd := range clusters.Items {
+		if _, ok := regionCredNames[childCd.Spec.Credential]; ok {
+			childCds = append(childCds, &childCd)
+		}
+	}
+
+	return childCds, nil
+}
+
+func GetRegionByClusterDeployment(ctx context.Context, kubeClient client.Client, cd *kcmv1beta1.ClusterDeployment, cache CachedClusterData) (*kcmv1beta1.Region, error) {
+	var err error
+	regionList := cache.Regions
+
+	if regionList == nil {
+		regionList, err = GetRegions(ctx, kubeClient)
+		if err != nil {
+			return nil, fmt.Errorf("failed to list regions: %v", err)
+		}
+	}
+
+	secretName, err := GetKubeconfigSecretName(ctx, kubeClient, cd)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get secret name for cluster deployment %s/%s: %v", cd.Namespace, cd.Name, err)
+	}
+
+	for _, region := range regionList.Items {
+		if region.Spec.ClusterDeployment != nil &&
+			region.Spec.ClusterDeployment.Name == cd.Name &&
+			region.Spec.ClusterDeployment.Namespace == cd.Namespace {
+			return &region, nil
+		}
+
+		if region.Spec.KubeConfig != nil && region.Spec.KubeConfig.Name == secretName {
+			return &region, nil
+		}
+	}
+
+	return nil, nil
+}
+
+// GetKcmRegionClusters returns the list of ClusterDeployments that are used as a KCM region.
+func GetKcmRegionClusters(ctx context.Context, kubeClient client.Client) ([]*kcmv1beta1.ClusterDeployment, error) {
+	clusterList, err := GetClusterDeployments(
+		ctx,
+		kubeClient,
+		&client.ListOptions{
+			LabelSelector: labels.Set{
+				kofClusterRoleLabel: kofRoleRegional,
+				kofKcmRegionLabel:   utils.True,
+			}.AsSelector(),
+		},
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	clusters := make([]*kcmv1beta1.ClusterDeployment, 0, len(clusterList.Items))
+	for i := range clusterList.Items {
+		clusters = append(clusters, &clusterList.Items[i])
+	}
+	return clusters, nil
+}
+
+func GetCredentials(ctx context.Context, kubeClient client.Client) (*kcmv1beta1.CredentialList, error) {
+	credList := new(kcmv1beta1.CredentialList)
+	err := kubeClient.List(ctx, credList)
+	return credList, err
+}
+
+func GetRegions(ctx context.Context, kubeClient client.Client) (*kcmv1beta1.RegionList, error) {
+	regionList := new(kcmv1beta1.RegionList)
+	err := kubeClient.List(ctx, regionList)
+	return regionList, err
 }
